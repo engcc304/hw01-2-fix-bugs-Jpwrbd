2cc065f240d05a943dc6cc19e0ec7514306b3a5c
Papawarin Bussadee <119738757+Jpwrbd@users.noreply.github.com>
2023-06-22 23:46:08 +0700

    Update hw01-3-basic-input-output.cpp

--- Homework/HW01/hw01-3-basic-input-output.cpp
+++ Homework/HW01/hw01-3-basic-input-output.cpp
@@ -11,5 +11,17 @@
         First Name: ABC
         Last Name: DEF
     Output:
-        ABD DEFs TC, RMUTL, Chiang Mai, Thailand
+        ABD DEF TC, RMUTL, Chiang Mai, Thailand
 */
+
+#include <stdio.h>
+int main() {
+    char firstName[] = "Aomsin";
+    char lastName[] = "AomMoney";
+    char address[] = "TC, RMUTL, Chiang Mai, Thailand";
+    char firstName2[] = "ABC";
+    char lastName2[] = "DEF";
+    char address2[] = "TC, RMUTL, Chiang Mai, Thailand";
+    printf("Output: %s %s %s\n", firstName, lastName, address);
+    printf("Output: %s %s %s\n", firstName2, lastName2, address2);
+}
